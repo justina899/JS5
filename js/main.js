b29f6a4195d8a665ceb5d685d90e6464c2de29be
Justina <justina.babrauskiene@gmail.com>
2022-11-03 09:48:51 +0200

    last

---
+++ js/main.js
@@ -0,0 +1,74 @@
+console.log("Funkcijos");
+console.log("1 uzd");
+console.log("5 ir 6 sandauga: " + sandauga(5, 6));
+function sandauga(num1, num2) {
+    return num1 * num2;
+}
+
+console.log("2 uzd");
+let vardas = "Jonas";
+labas(vardas);
+function labas(vardas) {
+    console.log("labas " + vardas);
+}
+
+console.log("3 uzd");
+tekstas("abrakadabra");
+function tekstas(text) {
+    console.log(text.length);
+}
+
+console.log("4 uzd");
+inicialai("vardas", "pavarde");
+function inicialai(v, p) {
+    console.log(v.charAt(0).toUpperCase() + p.charAt(0).toUpperCase());
+}
+
+console.log("5 uzd");
+numToHTML("10000");
+function numToHTML(num) {
+    document.getElementById("numberPlace").innerText = num;
+}
+//6 raudonas
+console.log("7 uzd");
+randNum();
+function randNum() {
+    let num1 = Math.round(Math.random() * 5);
+    let num2 = Math.round(Math.random() * 5);
+    let num3 = Math.round(Math.random() * 5);
+    console.log("atsitiktiniai skaiciai: " + num1 + ", " + num2 + ", " + num3);
+}
+
+console.log("8 uzd");
+console.log("random skaicius is intervalo [2, 9]: " + randNumFromRange(2, 9));
+function randNumFromRange(from, to) {
+    return (Math.round(Math.random() * (to - from)) + from);
+}
+
+console.log("9 uzd");
+kurtiP();
+function kurtiP() {
+    let HTML = "";
+    for (let i = 0; i < 10; i++) {
+        HTML += "<p>" + (i + 1).toString() + "</p>";
+    }
+    document.getElementById("sequence").innerHTML = HTML;
+    console.log("tagai sukurti");
+}
+
+console.log("VIDUTINIAI");
+console.log("1 uzd");
+console.log(pakeltiLaipsniu (5, 3));
+function pakeltiLaipsniu(skaicius, laipsnis) {
+    let rezultatas = 0;
+    for (let i = 1; i < laipsnis; i++) {
+        if(i = 1){
+            rezultatas += skaicius;
+        }
+        // if(i > 1){
+        //     rezultatas = rezultatas * skaicius;
+        // }
+        
+    }
+    return rezultatas;
+}
